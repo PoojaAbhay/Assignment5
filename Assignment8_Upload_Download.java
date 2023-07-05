5037861fb50b400395e1e092e2e7f9960da98172
Pooja Patil <95999587+PoojaAbhay@users.noreply.github.com>
2023-07-05 14:46:00 -0400

    Update Assignment8_Upload_Download.java

--- Assignment8_Upload_Download.java
+++ Assignment8_Upload_Download.java
@@ -9,14 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Assignment8_Upload_Download {
 
 	public static void main(String[] args) throws AWTException {
-		
-	
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\visit\\eclipse-workspace\\SeleniumPractice\\Driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\visit\\eclipse-workspace\\SeleniumPractice\\Driver\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-	
-		
-		
 		driver.get("https://demoqa.com/upload-download");
 		driver.manage().window().maximize();
 		
